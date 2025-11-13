cc479bc45f37e3176a039b6822d5068be036f187
skoopplayz <157137471+skoopplayz@users.noreply.github.com>
2025-11-13 18:23:32 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/beluga/',
-    bare:'https://wisp.rhw.one',
+    bare:'https://32867gd198764dg23vd23198746v4d69182736dvussis7.9882136.xyz/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
