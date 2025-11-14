a39a2f7d0cbb5939dad0f2e37c82cfc754e49c17
skoopplayz <157137471+skoopplayz@users.noreply.github.com>
2025-11-13 20:14:14 -0500

    Update search.js

--- static/search.js
+++ static/search.js
@@ -17,7 +17,7 @@ function search(input, template) {
   try {
     // input is a valid URL when http:// is added to the start:
     // eg: example.com, https://example.com/test?q=param
-    const url = new URL(`https://duckduckgo.com/?q=${input}`);
+    const url = new URL(`https://${input}`);
     // only if the hostname has a TLD/subdomain
     if (url.hostname.includes(".")) return url.toString();
   } catch (err) {
